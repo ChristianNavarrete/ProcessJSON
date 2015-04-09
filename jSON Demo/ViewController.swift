42a65c9da9888845eae44aa208e4bfca37d76e4f
HoodsDream <idinkyy@gmail.com>
2015-04-09 13:17:47 -0700

    initial commit

--- jSON Demo/ViewController.swift
+++ jSON Demo/ViewController.swift
@@ -13,6 +13,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url:NSURL = NSURL(string:"http://www.telize.com/geoip")!
+        
+        var session =  NSURLSession.sharedSession()
+        
+        let task = session.dataTaskWithURL(url, completionHandler: { data, response, error -> Void in
+            
+            println("task completed")
+            
+            if error != nil {
+                
+                println(error)
+                
+            }
+                
+            var err:NSError?
+                
+            var jsonReturn = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as  NSDictionary
+            
+            var ip:String = jsonReturn["ip"] as String
+                
+            println("My ip is \(ip)")
+
+            
+        })
+        
+        task.resume()
+        
     }
 
     override func didReceiveMemoryWarning() {
